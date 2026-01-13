855468a97008f909b6c6ad9aeff04ed75a6e0fed
Durgam Mohammed <durgam.mohammed@gmail.com>
2026-01-13 16:55:47 +0300

    ignore node_modules and env

---
+++ db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT || 3306),
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+module.exports = pool;
